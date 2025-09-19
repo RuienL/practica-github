6b0aa56d732ea871d81ff169d9a995b02ad12e1f
RuienL <109662778+RuienL@users.noreply.github.com>
2025-09-19 15:54:41 +0200

    python

--- EJERCICIO INICIAL/Ejercicio 12.py
+++ EJERCICIO INICIAL/Ejercicio 12.py
@@ -1,5 +1,10 @@
 lado=float(input("introduce el valor numérico de un lado del trapecio isóceles"))
-perimetro=lado*4
-area=lado**2
-print("el perímetro es de",perimetro)
-print("el area es de",area)
+basemayor=float(input("introduce el valor numérico de un base mayor del trapecio isóceles"))
+basemenor=float(input("introduce el valor numérico de un base menor del trapecio isóceles"))
+altura=float(input("introduce el valor numérico de una altura del trapecio isóceles"))
+area=(basemayor+basemenor)*altura/2
+perimetro=basemayor+basemenor+2*lado
+print("el área del trapecio isóceles es:",area)
+print("el perímetro del trapecio isóceles es:",perimetro)
+#Ejercicio 12: Calcular el área y perímetro de un trapecio isóceles
+#(se proporcionan la longitud de un lado, la base mayor, la base menor y la altura).
---
+++ EJERCICIO INICIAL/Ejercicio 13.py
@@ -0,0 +1,6 @@
+lado=float(input("introduce el valor numérico de un lado del volumen del cilindro"))
+volumen=lado**3
+area=6*lado**2
+print("el volumen del cubo es:",volumen)
+print("el área del cubo es:",area)
+#Ejercicio 13: Calcular el volumen y área de un cubo (se proporciona la longitud de una arista).
