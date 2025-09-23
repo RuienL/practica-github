c6410c5687ec20cea52f676fb1483a871d331f03
RuienL <109662778+RuienL@users.noreply.github.com>
2025-09-23 10:30:28 +0200

    PYTHON

---
+++ EJERCICIO INICIAL/Ejercicio 17.py
@@ -0,0 +1,7 @@
+peso=float(input("introduce tu peso en kg:"))
+altura=float(input("introduce tu altura en metros:"))
+IMC=peso/altura**2
+print("tu IMC es:",IMC)
+if IMC>=25:
+    print("tienes sobrepeso")
+
