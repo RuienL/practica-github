9fbd7583d11577e6e3db7b9f9580d5f36a47c2c0
RuienL <109662778+RuienL@users.noreply.github.com>
2025-09-23 10:42:02 +0200

    Update Ejercicio 18.py

---
+++ EJERCICIO INICIAL/Ejercicio 18.py
@@ -0,0 +1,9 @@
+precio=12
+menor=int(input("introduce el número de los menores de 18 años:"))
+mayor=int(input("intrtodcue el número de los mayores de 18 años:"))
+precio_menor=precio*0.5
+precio_mayor=precio*0.9
+total_menor=menor*precio_menor
+total_mayor=mayor*precio_mayor
+print("el precio total de cine de",menor,"menores de 18 años es:",total_menor)
+print("el precio total de cine de",mayor,"mayores de 18 años es:",total_mayor)
