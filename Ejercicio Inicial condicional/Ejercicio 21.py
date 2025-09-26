46973f50b675bdad621ad1f1b7961c2de17b975c
RuienL <109662778+RuienL@users.noreply.github.com>
2025-09-26 15:54:49 +0200

    Create Ejercicio 21.py

---
+++ Ejercicio Inicial condicional/Ejercicio 21.py
@@ -0,0 +1,12 @@
+import math
+a=float(input("Ingrese el valor de a: "))
+b=float(input("Ingrese el valor de b: "))
+c=float(input("Ingrese el valor de c: "))
+discriminante=b**2-4*a*c
+if discriminante>0:
+    raiz1=(-b+math.sqrt(discriminante))/(2*a)
+    raiz2=(-b-math.sqrt(discriminante))/(2*a)
+    print("el valor x1 es:",raiz1)
+    print("el valor x2 es:",raiz2)
+else:
+    print("La ecuacion no tiene soluciones reales")
