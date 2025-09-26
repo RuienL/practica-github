5b9f2374fbdf02d4a737ab0e855c4a8c40952221
RuienL <109662778+RuienL@users.noreply.github.com>
2025-09-26 15:33:48 +0200

    python

--- EJERCICIO INICIAL/Ejercicio 18.py
+++ EJERCICIO INICIAL/Ejercicio 18.py
@@ -1,8 +1,10 @@
 precio=12
 menor=int(input("introduce el número de los menores de 18 años:"))
 mayor=int(input("intrtodcue el número de los mayores de 18 años:"))
-precio_menor=precio*0.5
+precio_menor=precio*0.5, 
+round(precio_menor,3)
 precio_mayor=precio*0.9
+round(precio_mayor,3)
 total_menor=menor*precio_menor
 total_mayor=mayor*precio_mayor
 print("el precio total de cine de",menor,"menores de 18 años es:",total_menor)
---
+++ Ejercicio Inicial condicional/Ejercicio 19.py
@@ -0,0 +1,8 @@
+num1=float(input("Ingrese el primer numero: "))
+num2=float(input("Ingrese el segundo numero: "))
+if num1>num2:
+    print("El",num1, "es mayor")
+elif num1<num2:
+    print("El",(int(num2,)) "es mayor")
+else:
+    print("Los numeros son iguales")
