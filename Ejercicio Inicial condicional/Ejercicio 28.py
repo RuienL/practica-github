2fff1544337e311839f1fafcdd3b97da11ed7c31
RuienL <109662778+RuienL@users.noreply.github.com>
2025-10-08 09:54:45 +0200

    Update Ejercicio 28.py

--- Ejercicio Inicial condicional/Ejercicio 28.py
+++ Ejercicio Inicial condicional/Ejercicio 28.py
@@ -2,9 +2,9 @@
 var1 = (input("Introduce un carácter: "))
 if var1.isupper()==True:
     print("El carácter es una letra mayúscula")
-if var1.islower()==True:
+elif var1.islower()==True:
     print("El carácter es una letra minúscula")
-if var1.isdigit()==True:
+elif var1.isdigit()==True:
     print("El carácter es un número")
-elif not var1.isalnum():
+elif not var1.isalnum()==False:
     print("El carácter es un símbolo")
